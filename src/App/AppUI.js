c58c8206772a7914ce067941a7570afca3e005b7
Hugo <hugotranquilino@gmail.com>
2022-01-11 13:37:46 -0600

    Comienzo con la estructuracion para la UI del sitio

---
+++ src/App/AppUI.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Search } from "../Search";
+
+function AppUI() {
+    return(
+        <React.Fragment>
+            <Search />
+            <h1> hello world</h1>
+        </React.Fragment>
+    );
+}
+
+export { AppUI };
