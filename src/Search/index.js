4097e836d1d2f13577cb6d50121846257d5d3233
Hugo <hugotranquilino@gmail.com>
2022-01-11 13:38:50 -0600

    maquetado de search para la busqueda de films

---
+++ src/Search/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Search.css';
+
+function Search() {
+    return(
+        <input
+            className="search"
+            placeholder="Buscar ..."
+
+        />
+    );
+}
+
+export { Search }
