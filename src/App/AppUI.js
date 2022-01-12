b36431e13390ba89ac3e4fcbc5f4a4805d6e1678
Hugo <hugotranquilino@gmail.com>
2022-01-11 23:40:07 -0600

    agregue el logo de studios ghibli

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { Search } from "../Search";
+import { Search } from "../components/Search";
+import logo from '../assets/img/Studio_Ghibli.png';
+import { Cards } from '../components/Cards' 
 
 function AppUI() {
     return(
         <React.Fragment>
+            <img 
+                className="ghibli_logo"
+                src={logo} 
+                alt="ghibli films"
+            />
             <Search />
             <h1> hello world</h1>
+            <Cards/>
         </React.Fragment>
     );
 }
