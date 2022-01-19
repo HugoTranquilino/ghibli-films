f8a6c41416b0a43abcd3d81b9fcec512a7f653c8
Hugo <hugotranquilino@gmail.com>
2022-01-19 14:44:39 -0600

    trabaje una pagina 404 para cuando el usuario accede a una url que no se encuetra dentro de las rutas del proyecto

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import imagen from "../image/404.png"
+import "../styles/pageNotFound.css"
 
 function PageNotFound() {
     console.log('404')
     return(
-        <h1>Page Not Found</h1>
+        <div className="container">
+            <section className="section__notFound">
+                <img src={imagen} className="img-notfound" />
+                <section className="set__info">
+                    <h1 className="section__notFound-Oops">Ooops...</h1>
+                    <p className="section__notFound-subtitle">No se ha encontrado página solicitada</p>
+                </section>
+                <Link to="/" className="btn-home">Regresar al Inicio</Link>
+            </section>
+        </div>
     );
 }
 
