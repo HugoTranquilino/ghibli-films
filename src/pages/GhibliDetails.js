e482856f7f56edfaccf6fb48f32866b78d5ee459
Hugo <hugotranquilino@gmail.com>
2022-01-18 18:38:40 -0600

    trabaje la busqueda para el detalle del film

--- src/pages/GhibliDetails.js
+++ src/pages/GhibliDetails.js
@@ -25,26 +25,31 @@ function GhibliDetails(props) {
     
     return(
         <section className="container__details">
-            <section className="details__banner">
-                <img className="details__banner-img" src={filmsDetail.movie_banner}/>
+            <section className="target">
+                <img className="target--image" src={filmsDetail.image}/>
                 <h1 className="details__banner-title">{filmsDetail.title}</h1>
             </section>
-            
+
             <section className="section-info">
-                <h2 className="details-age">Año de lanzamiento: {filmsDetail.release_date}</h2>
+
+                <section className="details__banner">
+                    <img className="details__banner-img" src={filmsDetail.movie_banner}/>
+                </section>
+                <h2 className="details-age">{filmsDetail.release_date}</h2>
 
                 <section className="container__info">
                     <section className="container__info-description">
                         <p className="description">{filmsDetail.description}</p>
                     </section>
                     <section className="container__info-film">
-                        <h3 className="details_subtitle">Director: {filmsDetail.director}</h3>
-                        <h3 className="details_subtitle">Productor: {filmsDetail.producer}</h3>
+                        <h3 className="details_director">{filmsDetail.director}</h3>
+                        <h3 className="details_producer">{filmsDetail.producer}</h3>
                     </section>
                 </section>
 
                 <section className="container__people">
                     {/* <p className="people_name">{filmsPeople.name}</p> */}
+
                 </section>
             </section>
         </section>
